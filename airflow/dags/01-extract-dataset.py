1b9747e5f75c6bd2cd802b170f7c32755c125eeb
João Pedro Valladão Pinheiro <joao.valladao@prof.infnet.edu.br>
2023-04-28 22:01:49 -0300

    Example reading CSV

--- airflow/dags/01-extract-dataset.py
+++ airflow/dags/01-extract-dataset.py
@@ -1,12 +1,8 @@
 import logging
+import pandas as pd
 import datetime as dt
 from airflow.models import DAG
-from airflow.utils.trigger_rule import TriggerRule
-from airflow.operators.dummy import DummyOperator
-# from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-# from airflow.providers.sqlite.operators.sqlite import SqliteOperator
-# from airflow.providers.sqlite.hooks.sqlite import SqliteHook
-# from utils.helper import apply_task_downstream
+from airflow.decorators import task
 
 
 log = logging.getLogger(__name__)
@@ -31,9 +27,7 @@ DEFAULT_ARGS = {
     # 'trigger_rule': 'all_success'
 }
 
-sequence = []
-
-dag = DAG(
+with DAG(
     '01_extract_dataset',
     default_args=DEFAULT_ARGS,
     description='A simple tutorial DAG',
@@ -41,36 +35,15 @@ dag = DAG(
     start_date=dt.datetime(2021, 1, 1),
     catchup=False,
     tags=['example'],
-)
+) as dag:
 
-#extract_artists_sqlite_task = SqliteOperator(
-#    task_id='extract_artists',
-#    sql='select * from artist',  # can refer to a file
-#    sqlite_conn_id='chinook_db',
-#    dag=dag,
-#)
-#sequence.append(extract_artists_sqlite_task)
+    @task
+    def extract_drivers():
+        df = pd.read_csv("dags/input/drivers.csv", encoding="iso-8859-1")
+        return df.iloc[0]["driverRef"]
 
-conn = DummyOperator(
-    task_id='connector',
-    trigger_rule=TriggerRule.ALL_DONE,  # all directly upstream tasks are finished - failed or succeeded
-    dag=dag,
-)
-sequence.append(conn)
+    @task
+    def print_log(driver_name):
+        log.info(f"====> Driver Name: {driver_name}")
 
-#for table_name in ['Artist', 'Album']:
-#    @dag.task(task_id=f"extract_table_{table_name}")
-#    def extract_table():
-#        sqlite_hook = SqliteHook(sqlite_conn_id='chinook_db')
-#        df = sqlite_hook.get_pandas_df(sql=f'select * from {table_name}')
-#        df.to_csv(f'/opt/airflow/outputs/{table_name}.csv', index=False)
-#    sequence.append(extract_table())
-
-#trigger_transformation = TriggerDagRunOperator(
-#    task_id='trigger_transformation_dag',
-#    trigger_dag_id='02_transform_data',
-#    dag=dag,
-#)
-#sequence.append(trigger_transformation)
-
-#apply_task_downstream(sequence)
+    print_log(extract_drivers())
