0b5fe5c3c367acd34765e09f1189493113b06924
João Pedro Valladão Pinheiro <joao.valladao@prof.infnet.edu.br>
2023-05-12 22:12:01 -0300

    Applying hive and spark interpreters modifications

--- script_files/bootstrap.sh
+++ script_files/bootstrap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 hdfs namenode -format
 service ssh start
 if [ "$HOSTNAME" = node-master ]; then
@@ -7,10 +6,10 @@ if [ "$HOSTNAME" = node-master ]; then
     start-yarn.sh
     # start-master.sh
     # /opt/hive/bin/schematool -dbType postgres -initSchema
-    # hive --service hiveserver2 &
-    hive --service metastore &
-    cd /root/lab
-    jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' &
+    # hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10000 --hiveconf hive.root.logger=DEBUG,console &
+    # hive --service metastore &
+    # cd /root/lab
+    # jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' &
 fi
 #bash
 while :; do :; done & kill -STOP $! && wait $!
