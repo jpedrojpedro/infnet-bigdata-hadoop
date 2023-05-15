548f6ddee837c9aa5211b2aeb17554682d2a0a10
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2023-05-15 19:24:17 -0300

    Small adjustments

--- hadoop-with-jupyter/bootstrap.sh
+++ hadoop-with-jupyter/bootstrap.sh
@@ -5,15 +5,24 @@ if [ "$HOSTNAME" = node-master ]; then
     start-dfs.sh
     start-yarn.sh
     # start-master.sh
-    # configuring postgresql as hive metastore
-     /opt/hive/bin/schematool -dbType postgres -initSchema
-     # starting hive as metastore
-     hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10000 --hiveconf hive.root.logger=DEBUG,console &
-     # starting hive as query engine
-     hive --service metastore &
-     cd /root/lab
-     # starting jupyter notebook
-     jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' &
+    echo "########## waiting postgresql start"
+    sleep 30
+    FILE=/root/.metastore
+    if [ -f "$FILE" ]; then
+        echo "########## $FILE exists - skipping metastore configuration"
+    else
+        echo "########## configuring postgresql as hive metastore"
+        /opt/hive/bin/schematool -dbType postgres -initSchema
+        echo "########## persisting .metastore control file"
+        echo "hive-metastore" >> "$FILE"
+    fi
+    echo "########## starting hive as metastore"
+    hive --service metastore &
+    echo "########## starting hive as query engine"
+    hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10000 --hiveconf hive.root.logger=DEBUG,console &
+    cd /root/lab
+    echo "########## starting jupyter notebook"
+    jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password='' &
 fi
 #bash
 while :; do :; done & kill -STOP $! && wait $!
